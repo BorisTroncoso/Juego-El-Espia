62504c0a258fadd3add41619856b9831f9c759a7
BorisTroncoso <150861586+BorisTroncoso@users.noreply.github.com>
2026-03-06 17:16:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const { Server } = require('socket.io');
 
 const app = express();
 app.use(express.static(__dirname)); 
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+
 
 
 const server = http.createServer(app);
